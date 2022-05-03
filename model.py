cdf0efe19781b0f29b9f8e4d89aa76b4d50b0d80
akozlova <annakozlova92@gmail.com>
2022-05-03 02:06:36 +0000

    update connect_to_db to add flask

--- model.py
+++ model.py
@@ -60,17 +60,24 @@ class Pups(db.Model):
     def __repr__(self):
         return f"<Pups pups_id={self.pups_id}>"
 
-
-def connect_to_db(app, db_name):
+def connect_to_db(flask_app, db_uri="postgresql:///miceTrack", echo=True):
     """Connect to database."""
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{db_name}"
-    app.config["SQLALCHEMY_ECHO"] = True
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.app = app
-    db.init_app(app)
+    db.app = flask_app
+    db.init_app(flask_app)
 
-app = Flask(__name__)
+    print("Connected to the db!")
 
-connect_to_db(app, 'micetrack')
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
