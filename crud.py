2a4a47fe193df3bfcf7150de458e3064be54b930
akozlova <annakozlova92@gmail.com>
2022-05-04 03:50:55 +0000

    add html template and route for homepage

--- crud.py
+++ crud.py
@@ -11,34 +11,35 @@ def create_user(email, name):
     return user
 
 
-def create_female_mouse(mating_date, days_in_breeding, check_pregnancy, pregnant):
-    """Create and return a new female mice."""
+# def create_female_mouse(mating_date, days_in_breeding, check_pregnancy, pregnant):
+#     """Create and return a new female mice."""
 
-    female_mouse = FemaleMouse(
-        mating_date=mating_date, 
-        days_in_breeding=days_in_breeding,
-        check_pregnancy=check_pregnancy,
-        pregnant=pregnant,
-    )
+#     female_mouse = FemaleMouse(
+#         mating_date=mating_date,
+#         days_in_breeding=days_in_breeding,
+#         check_pregnancy=check_pregnancy,
+#         pregnant=pregnant
+#     )
 
-    return female_mouse
+#     return female_mouse
 
 
-def create_pup(date_of_birth, pup_strain, days_old, wean_date, need_to_id):
-    """ Create and return pup """
+# def create_pup(date_of_birth, pup_strain, days_old, wean_date, need_to_id):
+#     """ Create and return pup """
     
-    pup = Pup(
-        date_of_birth=date_of_birth,
-        pup_strain=pup_strain,
-        days_old=days_old,
-        wean_date=wean_date,
-        need_to_id=need_to_id,
-    )
+#     pup = Pup(
+#         date_of_birth=date_of_birth,
+#         pup_strain=pup_strain,
+#         days_old=days_old,
+#         wean_date=wean_date,
+#         need_to_id=need_to_id,
+#     )
 
-    return pup
+#     return pup
 
 # do i need to create litter funciton?
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from server import app
+
     connect_to_db(app)
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 """Server for miceTrack  app."""
 
-from flask import Flask
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+import crud
 
 app = Flask(__name__)
 
 
-# Replace this with routes and view functions!
+@app.route("/")
+def homepage():
+    """View homepage."""
 
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
-    # DebugToolbarExtension(app)
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
