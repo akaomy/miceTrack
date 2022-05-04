16d20d4f83d7f7f84c97c39411746c87a1873390
akozlova <annakozlova92@gmail.com>
2022-05-04 00:18:30 +0000

    add create user, female_mouse and pup functions

--- crud.py
+++ crud.py
@@ -1,6 +1,43 @@
 """CRUD operations."""
 
-from model import db, User, Movie, Rating, connect_to_db
+from model import db, FemaleMouse, Pup, Litter, User, connect_to_db
+
+# get user info from google sign in?
+def create_user(email, name):
+    """Create and return a new user."""
+    
+    user = User(email=email, name=name)
+
+    return user
+
+
+def create_female_mouse(mating_date, days_in_breeding, check_pregnancy, pregnant):
+    """Create and return a new female mice."""
+
+    female_mouse = FemaleMouse(
+        mating_date=mating_date, 
+        days_in_breeding=days_in_breeding,
+        check_pregnancy=check_pregnancy,
+        pregnant=pregnant,
+    )
+
+    return female_mouse
+
+
+def create_pup(date_of_birth, pup_strain, days_old, wean_date, need_to_id):
+    """ Create and return pup """
+    
+    pup = Pup(
+        date_of_birth=date_of_birth,
+        pup_strain=pup_strain,
+        days_old=days_old,
+        wean_date=wean_date,
+        need_to_id=need_to_id,
+    )
+
+    return pup
+
+# do i need to create litter funciton?
 
 if __name__ == '__main__':
     from server import app
