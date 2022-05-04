cbedd3583dd9f7a6aa7cedcbb344d0be5b5aa856
akozlova <annakozlova92@gmail.com>
2022-05-04 18:18:52 +0000

    add html for tracking table and route to display this page

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 """Server for miceTrack  app."""
 
-from flask import Flask, render_template, request, flash, session, redirect
+from flask import (Flask, render_template, request, flash, session, redirect)
 from model import connect_to_db, db
 import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route("/")
@@ -12,6 +15,14 @@ def homepage():
     """View homepage."""
 
     return render_template("homepage.html")
+
+
+@app.route('/mice-tracking')
+def display_micetrack_table():
+    """Track a mice table"""
+
+    return render_template('mice-tracking-table.html')
+
 
 if __name__ == "__main__":
     connect_to_db(app)
