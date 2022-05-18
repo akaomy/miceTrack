751afe3ff9cc5c00ec9f0e33b9cb7ce3898a6e21
akozlova <annakozlova92@gmail.com>
2022-05-18 03:54:47 +0000

    add update_female_row_data funciton

--- crud.py
+++ crud.py
@@ -2,7 +2,6 @@
 
 from model import db, FemaleMouse, Pup, Litter, User, connect_to_db
 
-# get user info from google sign in?
 def create_user(email, name):
     """Create and return a new user."""
     
@@ -26,12 +25,30 @@ def create_female_mouse(mating_date, days_in_breeding, check_pregnancy, pregnant
 
     return female_mouse
 
+def update_female_row_data(female_mouse_id, mating_date, days_in_breeding, check_pregnancy, pregnant):
+    
+    mouse = get_female_row_data(female_mouse_id)
+    
+    if not mouse:
+        raise ValueError("Mouse doesn't exist")
+
+    mouse.mating_date = mating_date
+    mouse.days_in_breeding = days_in_breeding
+    mouse.check_pregnancy = check_pregnancy
+    mouse.pregnant = pregnant
+
+    db.session.commit()
+
 
 def get_all_female_mice():
     """Return all female mice."""
 
     return FemaleMouse.query.all()
 
+def get_female_row_data(mouse_id):
+    """Get a female mice row data."""
+
+    return FemaleMouse.query.filter_by(FemaleMouse.female_mouse_id == mouse_id)
 
 def delete_female_row_data(mouse_id):
     """Delete a female mice row data."""
